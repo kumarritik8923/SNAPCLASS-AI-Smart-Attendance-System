7718ed3aad62e97b939291362cd747fa679d6605
Ritik Kumar Chhitrolia <ritikkumarchhitrolia@gmail.com>
2026-04-30 21:48:49 +0530

    streamlit setup + homescreen

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
 import streamlit as st
-
+from src.screens.home_screen import home_screen
+from src.screens.student_screen import student_screen
+from src.screens.teacher_screen import teacher_screen
 def main():
-    st.header("Learning Streamlit")
-    name = st.text_input('enter your name')
+    
+    if 'login_type' not in st.session_state:
+        st.session_state['login_type'] = None
 
-    col1,col2 = st.columns(2,gap = 'small')
+    match st.session_state['login_type']:
+        case 'teacher':
+            teacher_screen()
+        
+        case 'student':
+            student_screen()
 
-    with col1:
-        if st.button('Display my name',type='primary',key='btn1',width = 'stretch'):
-            print('hi ',name)
-    with col2:
-        if st.button('Display my name',type='secondary',key='btn2',width=300):
-            print('bye',name)
-    st.markdown("""
-    HI
-    """)
-    st.markdown("""
-        <style>
-            button{
-                background:orange !important;
-            }
-            </style>
-    """,unsafe_allow_html=True)
+        case None:
+            home_screen()
 
 main()
