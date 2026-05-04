9219e04fe4e1ed334c0df2adbff2e003573abd60
Ritik Kumar Chhitrolia <ritikkumarchhitrolia@gmail.com>
2026-05-04 17:42:44 +0530

    Voice+Face pipeline and Student dashboard

--- src/components/footer.py
+++ src/components/footer.py
@@ -10,3 +10,15 @@ def footer_home():
         </div>
                 
                 """,unsafe_allow_html=True)
+    
+
+def footer_dashboard():
+    
+
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; items-align:center; justify-content:center">
+        <p style="font-weight:bold; color:black"> Created with 💖 by </p>
+        <p style="font-weight:bold; color:#8B0000"> Ritik kumar chhitrolia </p>        
+        </div>
+                
+                """,unsafe_allow_html=True)
